31545db2065849a8174bd470a965940a95c61287
Nithin-ns24 <99644046+Nithin-ns24@users.noreply.github.com>
2024-04-09 18:32:36 +0530

    Update CalculatorUI.java

--- src/main/java/com/houarizegai/calculator/ui/CalculatorUI.java
+++ src/main/java/com/houarizegai/calculator/ui/CalculatorUI.java
@@ -156,14 +156,14 @@ public class CalculatorUI {
             typedValue = 0;
         });
 
-        btnBack = createButton("<-", columns[1], rows[1]);
+        btnBack = createButton("<-", columns[1], rows[1] + 1);
         btnBack.addActionListener(event -> {
             String str = inputScreen.getText();
             StringBuilder str2 = new StringBuilder();
             for (int i = 0; i < (str.length() - 1); i++) {
                 str2.append(str.charAt(i));
             }
-            if ("".equals(str2.toString())) {
+            if (str2.toString().equals("")) {
                 inputScreen.setText("0");
             } else {
                 inputScreen.setText(str2.toString());
